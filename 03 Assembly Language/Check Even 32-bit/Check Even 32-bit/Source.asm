bead8fa826ecbb8c7bc5e4990bba7e9e227630d3
Sangram Kesari Ray <sangram_kr@trendmicro.com>
2018-03-24 16:57:17 +0530

    update program for 32-bit

--- 03 Assembly Language/Check Even 32-bit/Check Even 32-bit/Source.asm
+++ 03 Assembly Language/Check Even 32-bit/Check Even 32-bit/Source.asm
@@ -5,16 +5,18 @@
 .stack 4096
 ExitProcess PROTO, dwExitCode:DWORD
 
+factor	EQU 2
+myDiv	TEXTEQU <mov ecx, factor>
+
 .data
 num1	DWORD 41
 num2	DWORD 50
-res		DWORd 0
 
 .code
 main PROC
-	mov edx, 0        ; clear dividend
-	mov eax, num2	  ; dividend
-	mov ecx, 2        ; divisor
+	mov edx, 0        
+	mov eax, num1	  
+	myDiv
 	div ecx           ; EAX contains the result of division, EDX contains the remainder
 
 	INVOKE ExitProcess, 0 ;program terminates with return 0
