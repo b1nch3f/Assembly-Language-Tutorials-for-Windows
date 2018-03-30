ea2d8d8c93cf290e562a15e6881b3c4094db1a01
Sangram Kesari Ray <sangram_kr@trendmicro.com>
2018-03-31 02:12:27 +0530

    uploaded chapter 6 - part 1

--- 06 Conditional Processing/Conditional Processing - Part 1/Conditional Processing - Part 1/Source.asm
+++ 06 Conditional Processing/Conditional Processing - Part 1/Conditional Processing - Part 1/Source.asm
@@ -14,7 +14,7 @@ main PROC
 	sahf
 
 	;PE(PARITY FLAG)
-	or al, 1010b
+	or al, 1001b
 	;ZE(ZERO FLAG)
 	and al, 0000b
 	;PL(SIGN FLAG)
@@ -26,6 +26,16 @@ main PROC
 	xor eax, eax
 	mov al, 01111111b
 	add al, 01000000b
+
+	xor eax, eax
+	mov ah, 00000000b
+	sahf
+
+	;TEST
+	mov	al, 1001b
+	test al, 1001b
+	;CMP
+	cmp al, 1011b
 
 	INVOKE ExitProcess, 0 ;program terminates with return 0
 main ENDP
